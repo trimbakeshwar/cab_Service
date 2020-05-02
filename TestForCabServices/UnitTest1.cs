2377a605ac1c1201bce559d44b593d04acda0864
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-05-02 19:10:58 +0530

    step3:-Enhancing Inovation

--- TestForCabServices/UnitTest1.cs
+++ TestForCabServices/UnitTest1.cs
@@ -18,9 +18,11 @@ namespace TestForCabServices
 
         public void TestForTotalFair()
         {
-            
+            //check for single cap services
             CabService service = new CabService(10, 15);
+            //total pay for single ride
             int totalpay = service.getTotalFair();
+            //if equel the pass
             Assert.AreEqual(115, totalpay);
         }
         /// <summary>
@@ -29,9 +31,13 @@ namespace TestForCabServices
         [Test]
         public void TestForMultipleRidesTotalFair()
         {
+            //array of distance
             int[] dist = { 10, 10, 10, 10 };
+            //array of minutes
             int[] min = { 15, 15, 15, 15 };
+            //create constructor for initialize and operaton
             MultipleRide ride = new MultipleRide(dist, min);
+            //total amount of multiple rides
             int totalAmunt = ride.totalFair;
             Assert.AreEqual(460, totalAmunt);
 
@@ -43,9 +49,14 @@ namespace TestForCabServices
         [Test]
         public void TestForMultipleRidesAggregateFair()
         {
+
+            //array of distance
             int[] dist = { 10, 10, 10, 10 };
+            //array of minutes
             int[] min = { 15, 15, 15, 15 };
+            //create constructor for initialize and operaton
             MultipleRide ride = new MultipleRide(dist, min);
+            //avg amount
             int avgamount = ride.aggregateOfTotalFair;
             Assert.AreEqual(115, avgamount);
         }
@@ -55,10 +66,15 @@ namespace TestForCabServices
         [Test]
         public void TestForMultipleRidesNumberOfRides()
         {
+            //array of distance
             int[] dist = { 10, 10, 10, 10 };
+            //array of minutes
             int[] min = { 15, 15, 15, 15 };
+            //create constructor for initialize and operaton
             MultipleRide ride = new MultipleRide(dist, min);
+            //total number of rides
             int totalride = ride.TotalNumberOfRides;
+            //check with expected output
             Assert.AreEqual(4, totalride);
 
         }
--- cabService/MultipleRide.cs
+++ cabService/MultipleRide.cs
@@ -7,32 +7,50 @@ namespace cabService
 {
     public class MultipleRide
     {
+        //store multiple rides
         ArrayList list = new ArrayList();
+        //geting total fair
         public int totalFair { get; }
+        //geting total aggregateOfTotalFair
         public int aggregateOfTotalFair { get; }
+        //geting TotalNumberOfRides
         public int TotalNumberOfRides { get; }
         public MultipleRide(int[] DistanceArray, int[] muinetArray)
         {
+            //add all dist and rides in array list by loop
+            //creating object of distance and muinet by using single cab service
             for (int i = 0; i < muinetArray.Length; i++)
             {
                 list.Add(new CabService(DistanceArray[i], muinetArray[i]));
             }
+            //getting multiple rides total amount
             totalFair = getMultipleTotalFair();
+            //total rides
             TotalNumberOfRides = list.Count;
+            //aggreget rides
             aggregateOfTotalFair = getaggregateMoney();
         }
-
+        /// <summary>
+        /// calculate total money of all rides
+        /// </summary>
+        /// <returns>total faers</returns>
         public int getMultipleTotalFair()
         {
             int Fair = 0;
+            //geting Total fair of individual object amount and add in fair affter all list fair 
             foreach(CabService data in list)
             {
                 Fair += data.TotalFair;
             }
             return Fair;
         }
+        /// <summary>
+        /// calculate aggregate fair
+        /// </summary>
+        /// <returns></returns>
         public int getaggregateMoney()
         {
+            //total amount divided by number of ride and calculaate
             int aggregateMoney = totalFair / TotalNumberOfRides;
             return aggregateMoney;
         }
